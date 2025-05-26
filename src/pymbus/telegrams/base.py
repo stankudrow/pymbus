863d0a2378a798dfd8954ffcdeacf6615087b060
Stanley Kudrow <stankudrow@yandex.ru>
2025-05-26 14:26:31 +0300

    Subclass TelegramContainer from Sequence ABC class. (#16)
    * subclass TelegramContainer from Sequence abc
    
    * add PR #16 into the CHANGELOG

--- src/pymbus/telegrams/base.py
+++ src/pymbus/telegrams/base.py
@@ -7,14 +7,15 @@ Glossary:
 - a telegram byte refers to the "int | TelegramField"
 """
 
-from collections.abc import Iterable, Iterator
+import sys
+from collections.abc import Iterable, Iterator, Sequence
 from functools import total_ordering
+from typing import Any
 
 from pymbus.exceptions import MBusValidationError
 from pymbus.utils import validate_byte
 
 
-@total_ordering
 class TelegramField(int):
     """The base "Field" class.
 
@@ -36,7 +37,7 @@ TelegramByteIterableType = TelegramBytesType | Iterator[TelegramByteType]
 
 
 @total_ordering
-class TelegramContainer:
+class TelegramContainer(Sequence):
     """The base class for Telegram containers.
 
     A telegram container consists of telegram fields.
@@ -60,15 +61,16 @@ class TelegramContainer:
             [TelegramField(ibyte) for ibyte in ibytes] if ibytes else []
         )
 
+    def __contains__(self, value: Any) -> bool:
+        return value in self._fields
+
     def __eq__(self, other: object) -> bool:
         sfields = self._fields
         if isinstance(other, TelegramContainer):
             other = other._fields
         return sfields == other
 
-    def __getitem__(
-        self, key: int | slice
-    ) -> "TelegramField | TelegramContainer":
+    def __getitem__(self, key: int | slice, /) -> Any | Sequence[Any]:
         if isinstance(key, int):
             return self._fields[key]
         return type(self)(self._fields[key])
@@ -88,3 +90,26 @@ class TelegramContainer:
     def __repr__(self) -> str:
         cls_name = type(self).__name__
         return f"{cls_name}({self._fields})"
+
+    def __reversed__(self) -> Iterator[TelegramField]:
+        yield from reversed(self._fields)
+
+    def index(self, value: Any, start: int = 0, stop: None | int = None) -> int:
+        """Return the first index of the value.
+
+        Raises
+        ------
+        ValueError
+            if the value is not present
+
+        Returns
+        -------
+        int
+        """
+        stop = sys.maxsize if stop is None else stop
+        return self._fields.index(TelegramField(value), start, stop)
+
+    def count(self, value: Any) -> int:
+        """Return the number of occurrences of value."""
+
+        return self._fields.count(TelegramField(value))
