e7b01d9b372782b029643e3f986c45ab0c9ab5e0
Stanley Kudrow <stankudrow@yandex.ru>
2025-06-02 17:34:42 +0300

    finish 0xFD code table -> update VIFTablet API

--- src/pymbus/codes/vif.py
+++ src/pymbus/codes/vif.py
@@ -1,4 +1,4 @@
-"""M-Bus Value Information Field Code module."""
+"""M-Bus Value Information Field Code(s) module."""
 
 from collections.abc import Mapping
 from dataclasses import dataclass
@@ -7,15 +7,15 @@ from enum import Enum
 from pymbus.telegrams.fields import (
     ValueInformationField as VIF,
 )
-from pymbus.telegrams.fields import (
-    ValueInformationFieldExtension as VIFE,
-)
 
 
 class VIFCodeKind(str, Enum):
-    """VIF code description(s)."""
+    """Categories/Kinds for VIF codes."""
 
+    # auxiliary meta categories
     unknown = ""
+    reserved = "reserved"
+    # kinds defined in the table
     energy = "energy"
     volume = "volume"
     mass = "mass"
@@ -31,41 +31,94 @@ class VIFCodeKind(str, Enum):
     pressure = "pressure"
     time_point = "time point"
     hca = "heat cost allocator"
-    reserved = "reserved"
     averaging_duration = "averaging duration"
     actuality_duration = "actuality duration"
     fabrication_no = "fabrication no"
     enhanced = "enhanced"
     bus_address = "bus address"
-    user = "user definable"
+    user_definable = "user definable"
     any = "any"
-    manufacturer = "manufacturer specific"
+    manufacturer_specific = "manufacturer specific"
+    # 0xFB extension
     cold_warm_temperature_limit = "cold/warm temperature limit"
     cumul_count_max_power = "cumulative count maximum power"
+    # 0xFD extension
+    currency = "currency"
+    access_number = "access number (transmission count)"
+    medium = "medium"
+    manufacturer = "manufacturer"
+    param_set_identification = "parameter set identification"
+    model_version = "model/version"
+    hardware_version = "hardware version"
+    firmware_version = "firmware version"
+    software_version = "software version"
+    customer_location = "customer location"
+    customer = "customer"
+    access_code_user = "access code user"
+    access_code_operator = "access code operator"
+    access_code_system_operator = "access code system operator"
+    access_code_developer = "access code developer"
+    password = "password"  # noqa: S105 (bandit)
+    error_flags_binary = "error flags (binary)"
+    error_mask = "error mask"
+    digital_output_binary = "digital output (binary)"
+    digital_input_binary = "digital input (binary)"
+    baudrate = "baudrate (baud)"
+    response_delay_time = "response delay time (bittimes)"
+    retry = "retry"
+    first_storage = "first storage"
+    last_storage = "flaststorage"
+    storage_block_size = "storage block size"
+    storage_interval = "storage interval"
+    duration_since_last_readout = "duration since last readout [seconds->days]"
+    tariff_start = "tariff start (datetime)"
+    tariff_duration = "tariff duration"
+    tariff_period = "tariff period"
+    no_vif = "-"
+    voltage = "voltage"
+    current = "current"
+    reset_counter = "reset counter"
+    cumul_counter = "cumulation counter"
+    control_signal = "control signal"
+    week_day = "day of week"
+    week_number = "number of week"
+    day_change_timepoint = "time point of day change"
+    param_activation_state = "state of parameter activation"
+    special_supplier_info = "special supplier information"
+    duration_since_last_cumulation = "duration since last cumulation"
+    operating_time_battery = "operating time battery"
+    battery_change_datetime = "date and time of battery change"
 
 
 class VIFCodeUnit(str, Enum):
     """VIF code (include extended) unit(s)."""
 
     unknown = ""
+    # units defined in the table
+    dimensionless = "-"
     watt_hour = "Wh"
     joule = "J"
     meter_cubic = "m^3"
     kilogram = "kg"
     second = "s"
+    minute = "min"
+    hour = "h"
+    day = "day"
+    month = "month"
+    year = "year"
     watt = "W"
     joule_per_hour = "J/h"
     meter_cubic_per_hour = "m^3/h"
     meter_cubic_per_minute = "m^3/min"
     meter_cubic_per_second = "m^3/s"
     kilogram_per_hour = "kg/h"
-    celsius = "C"
+    celsius = "°C"
     kelvin = "K"
     bar = "bar"
-    hca = "H.C.A. Units"
+    hca = "H.C.A. units"
     date = "date"  # for a time point
     datetime = "datetime"  # for a time point
-    # 0xFB extension VIF code extra units
+    # 0xFB extension
     mega_watt_hour = "MWh"
     giga_joule = "GJ"
     tonne = "t"
@@ -75,7 +128,13 @@ class VIFCodeUnit(str, Enum):
     american_gallon_per_hour = "(american gallon)/h"
     mega_watt = "MW"
     giga_joule_per_hour = "GJ/h"
-    fahrenheit = "F"
+    fahrenheit = "°F"
+    # 0xFD extension
+    currency_credit = "currency credit units"
+    currency_debit = "currency debit units"
+    baud = "baud"
+    volt = "V"
+    amper = "A"
 
 
 @dataclass
@@ -83,8 +142,8 @@ class VIFCode:
     """Value Information Code."""
 
     coef: int | float = 1
-    kind: str = VIFCodeKind.unknown
-    unit: str = VIFCodeUnit.unknown
+    kind: str | VIFCodeKind = VIFCodeKind.unknown
+    unit: str | VIFCodeUnit = VIFCodeUnit.unknown
 
 
 # for optimisation sake
@@ -850,7 +909,7 @@ _VIF_CODE_MAP: dict[int, VIFCode] = {
     # special purpose VIF codes
     0b0111_1100: VIFCode(
         # code=0x7C,
-        kind=VIFCodeKind.user
+        kind=VIFCodeKind.user_definable
     ),
     0b0111_1110: VIFCode(
         # code=0x7E,
@@ -858,7 +917,7 @@ _VIF_CODE_MAP: dict[int, VIFCode] = {
     ),
     0b0111_1111: VIFCode(
         # code=0x7F,
-        kind=VIFCodeKind.manufacturer
+        kind=VIFCodeKind.manufacturer_specific
     ),
 }
 
@@ -1199,6 +1258,328 @@ _VIF_CODE_FB_EXTENSION_MAP: dict[int, VIFCode] = {
 }
 
 
+_VIF_CODE_FD_EXTENSION_MAP: dict[int, VIFCode] = {
+    # E000_00nn - currency units (credit)
+    0b0000_0000: VIFCode(
+        coef=1e-3,
+        kind=VIFCodeKind.currency,
+        unit=VIFCodeUnit.currency_credit,
+    ),
+    0b0000_0001: VIFCode(
+        coef=1e-2,
+        kind=VIFCodeKind.currency,
+        unit=VIFCodeUnit.currency_credit,
+    ),
+    0b0000_0010: VIFCode(
+        coef=1e-1,
+        kind=VIFCodeKind.currency,
+        unit=VIFCodeUnit.currency_credit,
+    ),
+    0b0000_0011: VIFCode(
+        coef=1,
+        kind=VIFCodeKind.currency,
+        unit=VIFCodeUnit.currency_credit,
+    ),
+    # E000_01nn - currency units (debit)
+    0b0000_0100: VIFCode(
+        coef=1e-3,
+        kind=VIFCodeKind.currency,
+        unit=VIFCodeUnit.currency_debit,
+    ),
+    0b0000_0101: VIFCode(
+        coef=1e-2,
+        kind=VIFCodeKind.currency,
+        unit=VIFCodeUnit.currency_debit,
+    ),
+    0b0000_0110: VIFCode(
+        coef=1e-1,
+        kind=VIFCodeKind.currency,
+        unit=VIFCodeUnit.currency_debit,
+    ),
+    0b0000_0111: VIFCode(
+        coef=1,
+        kind=VIFCodeKind.currency,
+        unit=VIFCodeUnit.currency_debit,
+    ),
+    # E000_1000 - E000_1111 -> Enhanced Identification
+    0b0000_1000: VIFCode(kind=VIFCodeKind.access_number),
+    0b0000_1001: VIFCode(kind=VIFCodeKind.medium),
+    0b0000_1010: VIFCode(kind=VIFCodeKind.manufacturer),
+    0b0000_1011: VIFCode(kind=VIFCodeKind.param_set_identification),
+    0b0000_1100: VIFCode(kind=VIFCodeKind.model_version),
+    0b0000_1101: VIFCode(kind=VIFCodeKind.hardware_version),
+    0b0000_1110: VIFCode(kind=VIFCodeKind.firmware_version),
+    0b0000_1111: VIFCode(kind=VIFCodeKind.software_version),
+    # E001_0000 - E001_1001 - TC294 WGI req(uirement)s
+    0b0001_0000: VIFCode(kind=VIFCodeKind.customer_location),
+    0b0001_0001: VIFCode(kind=VIFCodeKind.customer),
+    0b0001_0010: VIFCode(kind=VIFCodeKind.access_code_user),
+    0b0001_0011: VIFCode(kind=VIFCodeKind.access_code_operator),
+    0b0001_0100: VIFCode(kind=VIFCodeKind.access_code_system_operator),
+    0b0001_0101: VIFCode(kind=VIFCodeKind.access_code_developer),
+    0b0001_0110: VIFCode(kind=VIFCodeKind.password),
+    0b0001_0111: VIFCode(kind=VIFCodeKind.error_flags_binary),
+    0b0001_1000: VIFCode(kind=VIFCodeKind.error_mask),
+    0b0001_1001: _reserved_vif_code,
+    0b0001_1010: VIFCode(kind=VIFCodeKind.digital_output_binary),
+    0b0001_1011: VIFCode(kind=VIFCodeKind.digital_input_binary),
+    0b0001_1100: VIFCode(kind=VIFCodeKind.baudrate, unit=VIFCodeUnit.baud),
+    0b0001_1101: VIFCode(kind=VIFCodeKind.response_delay_time),
+    0b0001_1110: VIFCode(kind=VIFCodeKind.retry),
+    0b0001_1111: _reserved_vif_code,
+    0b0010_0000: VIFCode(kind=VIFCodeKind.first_storage),
+    0b0010_0001: VIFCode(kind=VIFCodeKind.last_storage),
+    0b0010_0010: VIFCode(kind=VIFCodeKind.storage_block_size),
+    0b0010_0011: _reserved_vif_code,
+    # E010_000 - E010_1111 - Enhanced storage management
+    0b0010_0100: VIFCode(
+        coef=1, unit=VIFCodeUnit.second, kind=VIFCodeKind.storage_interval
+    ),
+    0b0010_0101: VIFCode(
+        coef=60, unit=VIFCodeUnit.second, kind=VIFCodeKind.storage_interval
+    ),
+    0b0010_0110: VIFCode(
+        coef=3600, unit=VIFCodeUnit.second, kind=VIFCodeKind.storage_interval
+    ),
+    0b0010_0111: VIFCode(
+        coef=86400, unit=VIFCodeUnit.second, kind=VIFCodeKind.storage_interval
+    ),
+    0b0010_1000: VIFCode(
+        coef=1, unit=VIFCodeUnit.month, kind=VIFCodeKind.storage_interval
+    ),
+    0b0010_1001: VIFCode(
+        coef=1, unit=VIFCodeUnit.year, kind=VIFCodeKind.storage_interval
+    ),
+    0b0010_1010: _reserved_vif_code,
+    0b0010_1011: _reserved_vif_code,
+    0b0010_1100: VIFCode(
+        coef=1,
+        unit=VIFCodeUnit.second,
+        kind=VIFCodeKind.duration_since_last_readout,
+    ),
+    0b0010_1101: VIFCode(
+        coef=60,
+        unit=VIFCodeUnit.second,
+        kind=VIFCodeKind.duration_since_last_readout,
+    ),
+    0b0010_1110: VIFCode(
+        coef=3600,
+        unit=VIFCodeUnit.second,
+        kind=VIFCodeKind.duration_since_last_readout,
+    ),
+    0b0010_1111: VIFCode(
+        coef=86400,
+        unit=VIFCodeUnit.second,
+        kind=VIFCodeKind.duration_since_last_readout,
+    ),
+    # Enhanced tariff management
+    0b0011_0000: VIFCode(
+        kind=VIFCodeKind.tariff_start, unit=VIFCodeUnit.datetime
+    ),
+    0b0011_0001: VIFCode(
+        coef=60, kind=VIFCodeKind.tariff_duration, unit=VIFCodeUnit.second
+    ),
+    0b0011_0010: VIFCode(
+        coef=3600, kind=VIFCodeKind.tariff_duration, unit=VIFCodeUnit.second
+    ),
+    0b0011_0011: VIFCode(
+        coef=86400, kind=VIFCodeKind.tariff_duration, unit=VIFCodeUnit.second
+    ),
+    0b0011_0100: VIFCode(
+        coef=1, kind=VIFCodeKind.tariff_period, unit=VIFCodeUnit.second
+    ),
+    0b0011_0101: VIFCode(
+        coef=60, kind=VIFCodeKind.tariff_period, unit=VIFCodeUnit.second
+    ),
+    0b0011_0110: VIFCode(
+        coef=3600, kind=VIFCodeKind.tariff_period, unit=VIFCodeUnit.second
+    ),
+    0b0011_0111: VIFCode(
+        coef=86400, kind=VIFCodeKind.tariff_period, unit=VIFCodeUnit.second
+    ),
+    0b0011_1000: VIFCode(
+        coef=1, kind=VIFCodeKind.tariff_period, unit=VIFCodeUnit.month
+    ),
+    0b0011_1001: VIFCode(
+        coef=1, kind=VIFCodeKind.tariff_period, unit=VIFCodeUnit.year
+    ),
+    0b0011_1010: VIFCode(
+        kind=VIFCodeKind.no_vif, unit=VIFCodeUnit.dimensionless
+    ),
+    0b0011_1011: _reserved_vif_code,
+    0b0011_1100: _reserved_vif_code,
+    0b0011_1101: _reserved_vif_code,
+    0b0011_1110: _reserved_vif_code,
+    0b0011_1111: _reserved_vif_code,
+    # Electrical units
+    0b0100_0000: VIFCode(
+        coef=1e-9, kind=VIFCodeKind.voltage, unit=VIFCodeUnit.volt
+    ),
+    0b0100_0001: VIFCode(
+        coef=1e-8, kind=VIFCodeKind.voltage, unit=VIFCodeUnit.volt
+    ),
+    0b0100_0010: VIFCode(
+        coef=1e-7, kind=VIFCodeKind.voltage, unit=VIFCodeUnit.volt
+    ),
+    0b0100_0011: VIFCode(
+        coef=1e-6, kind=VIFCodeKind.voltage, unit=VIFCodeUnit.volt
+    ),
+    0b0100_0100: VIFCode(
+        coef=1e-5, kind=VIFCodeKind.voltage, unit=VIFCodeUnit.volt
+    ),
+    0b0100_0101: VIFCode(
+        coef=1e-4, kind=VIFCodeKind.voltage, unit=VIFCodeUnit.volt
+    ),
+    0b0100_0110: VIFCode(
+        coef=1e-3, kind=VIFCodeKind.voltage, unit=VIFCodeUnit.volt
+    ),
+    0b0100_0111: VIFCode(
+        coef=1e-2, kind=VIFCodeKind.voltage, unit=VIFCodeUnit.volt
+    ),
+    0b0100_1000: VIFCode(
+        coef=1e-1, kind=VIFCodeKind.voltage, unit=VIFCodeUnit.volt
+    ),
+    0b0100_1001: VIFCode(
+        coef=1, kind=VIFCodeKind.voltage, unit=VIFCodeUnit.volt
+    ),
+    0b0100_1010: VIFCode(
+        coef=1e1, kind=VIFCodeKind.voltage, unit=VIFCodeUnit.volt
+    ),
+    0b0100_1011: VIFCode(
+        coef=1e2, kind=VIFCodeKind.voltage, unit=VIFCodeUnit.volt
+    ),
+    0b0100_1100: VIFCode(
+        coef=1e3, kind=VIFCodeKind.voltage, unit=VIFCodeUnit.volt
+    ),
+    0b0100_1101: VIFCode(
+        coef=1e4, kind=VIFCodeKind.voltage, unit=VIFCodeUnit.volt
+    ),
+    0b0100_1110: VIFCode(
+        coef=1e5, kind=VIFCodeKind.voltage, unit=VIFCodeUnit.volt
+    ),
+    0b0100_1111: VIFCode(
+        coef=1e6, kind=VIFCodeKind.voltage, unit=VIFCodeUnit.volt
+    ),
+    0b0101_0000: VIFCode(
+        coef=1e-12, kind=VIFCodeKind.current, unit=VIFCodeUnit.amper
+    ),
+    0b0101_0001: VIFCode(
+        coef=1e-11, kind=VIFCodeKind.current, unit=VIFCodeUnit.amper
+    ),
+    0b0101_0010: VIFCode(
+        coef=1e-10, kind=VIFCodeKind.current, unit=VIFCodeUnit.amper
+    ),
+    0b0101_0011: VIFCode(
+        coef=1e-9, kind=VIFCodeKind.current, unit=VIFCodeUnit.amper
+    ),
+    0b0101_0100: VIFCode(
+        coef=1e-8, kind=VIFCodeKind.current, unit=VIFCodeUnit.amper
+    ),
+    0b0101_0101: VIFCode(
+        coef=1e-7, kind=VIFCodeKind.current, unit=VIFCodeUnit.amper
+    ),
+    0b0101_0110: VIFCode(
+        coef=1e-6, kind=VIFCodeKind.current, unit=VIFCodeUnit.amper
+    ),
+    0b0101_0111: VIFCode(
+        coef=1e-5, kind=VIFCodeKind.current, unit=VIFCodeUnit.amper
+    ),
+    0b0101_1000: VIFCode(
+        coef=1e-4, kind=VIFCodeKind.current, unit=VIFCodeUnit.amper
+    ),
+    0b0101_1001: VIFCode(
+        coef=1e-3, kind=VIFCodeKind.current, unit=VIFCodeUnit.amper
+    ),
+    0b0101_1010: VIFCode(
+        coef=1e-2, kind=VIFCodeKind.current, unit=VIFCodeUnit.amper
+    ),
+    0b0101_1011: VIFCode(
+        coef=1e-1, kind=VIFCodeKind.current, unit=VIFCodeUnit.amper
+    ),
+    0b0101_1100: VIFCode(
+        coef=1, kind=VIFCodeKind.current, unit=VIFCodeUnit.amper
+    ),
+    0b0101_1101: VIFCode(
+        coef=1e1, kind=VIFCodeKind.current, unit=VIFCodeUnit.amper
+    ),
+    0b0101_1110: VIFCode(
+        coef=1e2, kind=VIFCodeKind.current, unit=VIFCodeUnit.amper
+    ),
+    0b0101_1111: VIFCode(
+        coef=1e3, kind=VIFCodeKind.current, unit=VIFCodeUnit.amper
+    ),
+    0b0110_0000: VIFCode(kind=VIFCodeKind.reset_counter),
+    0b0110_0001: VIFCode(kind=VIFCodeKind.cumul_counter),
+    0b0110_0010: VIFCode(kind=VIFCodeKind.control_signal),
+    0b0110_0011: VIFCode(kind=VIFCodeKind.week_day),
+    0b0110_0100: VIFCode(kind=VIFCodeKind.week_number),
+    0b0110_0101: VIFCode(kind=VIFCodeKind.day_change_timepoint),
+    0b0110_0110: VIFCode(kind=VIFCodeKind.param_activation_state),
+    0b0110_0111: VIFCode(kind=VIFCodeKind.special_supplier_info),
+    # E110_10pp - Duration since last cumulation
+    0b0110_1000: VIFCode(
+        coef=3600,
+        kind=VIFCodeKind.duration_since_last_cumulation,
+        unit=VIFCodeUnit.second,
+    ),
+    0b0110_1001: VIFCode(
+        coef=86400,
+        kind=VIFCodeKind.duration_since_last_cumulation,
+        unit=VIFCodeUnit.second,
+    ),
+    0b0110_1010: VIFCode(
+        coef=1,
+        kind=VIFCodeKind.duration_since_last_cumulation,
+        unit=VIFCodeUnit.month,
+    ),
+    0b0110_1011: VIFCode(
+        coef=1,
+        kind=VIFCodeKind.duration_since_last_cumulation,
+        unit=VIFCodeUnit.year,
+    ),
+    0b0110_1100: VIFCode(
+        coef=3600,
+        kind=VIFCodeKind.operating_time_battery,
+        unit=VIFCodeUnit.second,
+    ),
+    0b0110_1101: VIFCode(
+        coef=86400,
+        kind=VIFCodeKind.operating_time_battery,
+        unit=VIFCodeUnit.second,
+    ),
+    0b0110_1110: VIFCode(
+        coef=1,
+        kind=VIFCodeKind.operating_time_battery,
+        unit=VIFCodeUnit.month,
+    ),
+    0b0110_1111: VIFCode(
+        coef=1,
+        kind=VIFCodeKind.operating_time_battery,
+        unit=VIFCodeUnit.year,
+    ),
+    0b0111_0000: VIFCode(
+        coef=1,
+        kind=VIFCodeKind.battery_change_datetime,
+        unit=VIFCodeUnit.datetime,
+    ),
+    0b0111_0001: _reserved_vif_code,
+    0b0111_0010: _reserved_vif_code,
+    0b0111_0011: _reserved_vif_code,
+    0b0111_0100: _reserved_vif_code,
+    0b0111_0101: _reserved_vif_code,
+    0b0111_0110: _reserved_vif_code,
+    0b0111_0111: _reserved_vif_code,
+    0b0111_1000: _reserved_vif_code,
+    0b0111_1001: _reserved_vif_code,
+    0b0111_1010: _reserved_vif_code,
+    0b0111_1011: _reserved_vif_code,
+    0b0111_1100: _reserved_vif_code,
+    0b0111_1101: _reserved_vif_code,
+    0b0111_1110: _reserved_vif_code,
+    0b0111_1111: _reserved_vif_code,
+}
+
+
 def _get_vif_code(
     value: int, /, source: Mapping[int, VIFCode]
 ) -> None | VIFCode:
@@ -1234,10 +1615,34 @@ def _get_vif_code(
 class VIFTablet:
     """VIFCode Table(t) Manager class."""
 
-    def __call__(
-        self, value: int | VIF | VIFE, *, extension_byte: None | int = None
+    def get_vif_code(
+        self, value: int | VIF, *, extension_byte: None | int = None
     ) -> None | VIFCode:
+        """Return VIFCode according to `value` byte.
+
+        Parameters
+        ----------
+        value : int | VIF
+            a byte value that can match a certain VIF code
+        extension_byte : None | int, default None
+            get a VIFCode from an extended table
+            according to `extension_byte` value
+
+        Raises
+        ------
+        ValueError
+            if extension code is invalid
+
+        Returns
+        -------
+        None | VIFCode
+        """
         value = int(value)
+        if extension_byte is None:
+            return _get_vif_code(value, source=_VIF_CODE_MAP)
         if extension_byte == 0xFB:
             return _get_vif_code(value, source=_VIF_CODE_FB_EXTENSION_MAP)
-        return _get_vif_code(value, source=_VIF_CODE_MAP)
+        if extension_byte == 0xFD:
+            return _get_vif_code(value, source=_VIF_CODE_FD_EXTENSION_MAP)
+        msg = f"wrong extension_byte={extension_byte}"
+        raise ValueError(msg)
