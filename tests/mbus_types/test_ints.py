aa60257c873549a827cef9b1e61d046bf11909cc
Stanley Kudrow <stankudrow@reply.no>
2025-06-09 10:59:52 +0300

    group and clean tests on integers

--- tests/mbus_types/test_ints.py
+++ tests/mbus_types/test_ints.py
@@ -12,114 +12,91 @@ from pymbus.mbtypes import (
 )
 
 
-@pytest.mark.parametrize("endianness", [BIG_ENDIAN, LITTLE_ENDIAN])
-@pytest.mark.parametrize(
-    ("it", "answer"),
-    [
-        ([0b0000_0000], 0),
-        ([0b1000_0000], -128),
-        ([0b1000_0001], -127),
-        ([0b0111_1111], 127),
-        ([0b1111_1111], -1),
-        # (
-        #     # <255, 1>
-        #
-        #     # non-negative -> 1 & 0x80 = 0 (False)
-        #     # 11) 0 << 8 = 0
-        #     # 12) 0 + 0000_0001 = 1
-        #     # 21) 1 <<< 8 = 1_0000_0000 = 256
-        #     # 22) 256 + 1111_1111 = 256 + 255 = 511
-        #     511,
-        # ),
-        # (
-        #     # <255, 129>
-        #     [0b1111_1111, 0b1000_0001],
-        #     # non-negative -> 129 & 0x80 = 1
-        #     # 11) 0 << 8 = 0
-        #     # 12) 0 + (129 ^ 0xFF) = 0 + 0111_1110 = 126
-        #     # 21) 128 << 8 = 0111_1110_0000_0000 = 32256
-        #     # 22) 32256 + (255 ^ 0xFF) = 32256
-        #     # neg -> True -> (-32256) - 1 = -32257
-        #     -32257,
-        # ),
-    ],
-)
-def test_parse_int_byte(
-    it: Iterable, endianness: Literal["big", "little"], answer: int
-):
-    assert parse_int(bytes(it), byteorder=endianness) == answer
+class TestParseInt:
+    @pytest.mark.parametrize("endianness", [BIG_ENDIAN, LITTLE_ENDIAN])
+    @pytest.mark.parametrize(
+        ("it", "answer"),
+        [
+            ([0b0000_0000], 0),
+            ([0b1000_0000], -128),
+            ([0b1000_0001], -127),
+            ([0b0111_1111], 127),
+            ([0b1111_1111], -1),
+        ],
+    )
+    def test_parse_int_byte(
+        self, it: Iterable, endianness: Literal["big", "little"], answer: int
+    ):
+        assert parse_int(bytes(it), byteorder=endianness) == answer
+
+    @pytest.mark.parametrize(
+        ("it", "endianness", "answer"),
+        [
+            (
+                [0b1111_1111, 0b0000_0001],
+                # [0xFF, 0x01]
+                # 0xFF => minus (MSB is 1) -> work with 0x7F = 127
+                # ~0xFF = 0b0000_0000 -> 0x00 = 0
+                # ~0x01 = 0b1111_1110 -> 0xFE = 254
+                # Total: -((254 + 0) + 1) = -255
+                BIG_ENDIAN,
+                -255,
+            ),
+            (
+                [0b1111_1111, 0b0000_0001],
+                LITTLE_ENDIAN,
+                511,  # [0x01, 0xFF] = 256 + 255 = 511
+            ),
+        ],
+    )
+    def test_parse_int_bytes(
+        self, it: Iterable, endianness: Literal["big", "little"], answer: int
+    ):
+        assert parse_int(bytes(it), byteorder=endianness) == answer
+
+    def test_parse_int_empty(self):
+        with pytest.raises(MBusError):
+            parse_int(bytes([]))
 
 
-@pytest.mark.parametrize(
-    ("it", "endianness", "answer"),
-    [
-        (
-            [0b1111_1111, 0b0000_0001],
-            # [0xFF, 0x01]
-            # 0xFF => minus (MSB is 1) -> work with 0x7F = 127
-            # ~0xFF = 0b0000_0000 -> 0x00 = 0
-            # ~0x01 = 0b1111_1110 -> 0xFE = 254
-            # Total: -((254 + 0) + 1) = -255
-            BIG_ENDIAN,
-            -255,
-        ),
-        (
-            [0b1111_1111, 0b0000_0001],
-            LITTLE_ENDIAN,
-            511,  # [0x01, 0xFF] = 256 + 255 = 511
-        ),
-    ],
-)
-def test_parse_int_bytes(
-    it: Iterable, endianness: Literal["big", "little"], answer: int
-):
-    assert parse_int(bytes(it), byteorder=endianness) == answer
+class TestParseUint:
+    @pytest.mark.parametrize("endianness", [BIG_ENDIAN, LITTLE_ENDIAN])
+    @pytest.mark.parametrize(
+        ("it", "answer"),
+        [
+            ([0b0000_0000], 0),
+            ([0b1000_0000], 128),
+            ([0b1111_1111], 255),
+        ],
+    )
+    def test_parse_uint_byte(
+        self, it: Iterable, endianness: Literal["big", "little"], answer: int
+    ):
+        assert parse_uint(bytes(it), byteorder=endianness) == answer
 
+    @pytest.mark.parametrize(
+        ("it", "endianness", "answer"),
+        [
+            (
+                [0b1111_1111, 0b0000_0001],
+                BIG_ENDIAN,
+                65281,
+            ),
+            (
+                [0b1111_1111, 0b0000_0001],
+                LITTLE_ENDIAN,
+                511,
+            ),
+        ],
+    )
+    def test_parse_uint_bytes(
+        self, it: Iterable, endianness: Literal["big", "little"], answer: int
+    ):
+        assert parse_uint(bytes(it), byteorder=endianness) == answer
 
-def test_parse_int_empty():
-    with pytest.raises(MBusError):
-        parse_int(bytes([]))
-
-
-@pytest.mark.parametrize("endianness", [BIG_ENDIAN, LITTLE_ENDIAN])
-@pytest.mark.parametrize(
-    ("it", "answer"),
-    [
-        ([0b0000_0000], 0),
-        ([0b1000_0000], 128),
-        ([0b1111_1111], 255),
-    ],
-)
-def test_parse_uint_byte(
-    it: Iterable, endianness: Literal["big", "little"], answer: int
-):
-    assert parse_uint(bytes(it), byteorder=endianness) == answer
-
-
-@pytest.mark.parametrize(
-    ("it", "endianness", "answer"),
-    [
-        (
-            [0b1111_1111, 0b0000_0001],
-            BIG_ENDIAN,
-            65281,
-        ),
-        (
-            [0b1111_1111, 0b0000_0001],
-            LITTLE_ENDIAN,
-            511,
-        ),
-    ],
-)
-def test_parse_uint_bytes(
-    it: Iterable, endianness: Literal["big", "little"], answer: int
-):
-    assert parse_uint(bytes(it), byteorder=endianness) == answer
-
-
-def test_parse_uint_empty():
-    with pytest.raises(MBusError):
-        parse_uint(bytes([]))
+    def test_parse_uint_empty(self):
+        with pytest.raises(MBusError):
+            parse_uint(bytes([]))
 
 
 @pytest.mark.parametrize(
